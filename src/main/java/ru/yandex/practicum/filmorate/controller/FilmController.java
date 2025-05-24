89b4e700bf67dcc5cf92b3428f233cb36fda6fb0
Vitaly-Trepalin <vitalietrepalin@yandex.ru>
2025-05-24 09:57:34 +0300

    add classes: ErrorResponse, ErrorHandler, FilmService, UserService, FilmStorage, InMemoryFilmStorage, UserStorage, InMemoryUserStorage

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,90 +1,70 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.FilmService;
+import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 
-import java.time.LocalDate;
 import java.util.*;
 
 @RestController
 @RequestMapping("/films")
 @Slf4j
 public class FilmController {
-    private Map<Long, Film> films = new HashMap<>();
+    private final FilmService filmService;
+    private final FilmStorage filmStorage;
+
+    @Autowired
+    public FilmController(FilmService filmService, FilmStorage filmStorage) {
+        this.filmService = filmService;
+        this.filmStorage = filmStorage;
+    }
 
     @GetMapping
     public List<Film> findAll() {
-        log.info("Get all films");
-        return new ArrayList<>(films.values());
+        return filmStorage.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Film findById(@PathVariable Long id) {
+        return filmStorage.findById(id);
     }
 
     @PostMapping
     public Film create(@RequestBody Film film) {
-        validation(film);
-        film.setId(getNextId());
-        log.debug("Adding id (id={})", film.getId());
-        films.put(film.getId(), film);
-        log.info("Adding a movie id={}", film.getId());
-        return film;
+        return filmStorage.create(film);
     }
 
     @PutMapping
     public Film update(@RequestBody Film film) {
-        if (film.getId() == null) {
-            log.warn("Movie id not specified");
-            throw new ValidationException("Не указан id фильма");
-        }
-        if (films.containsKey(film.getId())) {
-            validation(film);
-            log.info("The film id={} has been updated", film.getId());
-            films.put(film.getId(), film);
-            return film;
+        return filmStorage.update(film);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        filmStorage.delete(id);
+    }
+
+    @PutMapping("/{id}/like/{userId}")
+    public void addLike(@PathVariable Long id,
+                        @PathVariable Long userId) {
+        filmService.addLike(id, userId);
+    }
+
+    @DeleteMapping("/{id}/like/{userId}")
+    public void removeLike(@PathVariable Long id,
+                           @PathVariable Long userId) {
+        filmService.removeLike(id, userId);
+    }
+
+    @GetMapping("/popular")
+    public List<Film> showPopularFilms(@RequestParam(required = false) Long count) {
+        if (count == null) {
+            return filmService.showPopularFilms();
         } else {
-            log.warn("There is no movie with id={}", film.getId());
-            throw new NotFoundException("Фильма с id=" + film.getId() + " нет");
-        }
-    }
-
-    private long getNextId() {
-        long maxId = films.keySet()
-                .stream()
-                .mapToLong(id -> id)
-                .max()
-                .orElse(0);
-        return ++maxId;
-    }
-
-    private void validation(Film film) {
-        if (film.getName() == null || film.getName().isBlank()) {
-            log.warn("The title of the film is not specified");
-            throw new ValidationException("Не указано название фильма");
-        }
-        if (film.getDescription() == null || film.getDescription().isBlank()) {
-            log.warn("No movie description provided");
-            throw new ValidationException("Не указано описание фильма");
-        }
-        if (film.getDescription().length() > 200) {
-            log.warn("The movie description contains more than 200 characters");
-            throw new ValidationException("Описание фильма превышает 200 символов");
-        }
-        if (film.getReleaseDate() == null) {
-            log.warn("The release date of the film is not specified");
-            throw new ValidationException("Не указана дата выхода фильма");
-        }
-        if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
-            log.warn("The release date of the film is incorrect");
-            throw new ValidationException("Дата выхода фильма неверная");
-        }
-        if (film.getDuration() == null) {
-            log.warn("The duration of the film is not specified");
-            throw new ValidationException("Не указана продолжительность фильма");
-        }
-        if (film.getDuration() < 0) {
-            log.warn("The length of a movie cannot be negative");
-            throw new ValidationException("Продолжительность фильма не может быть отртцательной");
+            return filmService.showPopularFilms(count);
         }
     }
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -0,0 +1,83 @@
+package ru.yandex.practicum.filmorate.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
+import ru.yandex.practicum.filmorate.storage.user.UserStorage;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@Service
+@Slf4j
+public class FilmService {
+    private final FilmStorage filmStorage;
+    private final UserStorage userStorage;
+
+    @Autowired
+    public FilmService(FilmStorage filmStorage, UserStorage userStorage) {
+        this.filmStorage = filmStorage;
+        this.userStorage = userStorage;
+    }
+
+    public void addLike(Long id, Long userId) {
+        log.info("Method started (addLike)");
+        Map<Long, Film> films = filmStorage.getFilms();
+        Map<Long, User> users = userStorage.getUsers();
+        if (!films.containsKey(id)) {
+            log.warn("Like not added. No movie id={}", id);
+            throw new NotFoundException("Лайк не поставлен. Нет фильма id=" + id);
+        }
+        if (!users.containsKey(userId)) {
+            log.warn("Like not added. No user id={}", id);
+            throw new NotFoundException("Лайк не поставлен. Нет пользователя id=" + userId);
+        }
+        Set<Long> likes = films.get(id).getLikes();
+        log.info("like added id={}", id);
+        likes.add(userId);
+    }
+
+    public void removeLike(Long id, Long userId) {
+        log.info("Method started (removeLike)");
+        Map<Long, Film> films = filmStorage.getFilms();
+        Map<Long, User> users = userStorage.getUsers();
+        if (!films.containsKey(id)) {
+            log.warn("Like not deleted. No film id={}", id);
+            throw new NotFoundException("Лайк не удален. Нет фильма id=" + id);
+        }
+        if (!users.containsKey(userId)) {
+            log.warn("Like not deleted. No user id={}", userId);
+            throw new NotFoundException("Лайк не удален. Нет пользователя id=" + userId);
+        }
+        Set<Long> likes = films.get(id).getLikes();
+        if (!likes.contains(userId)) {
+            log.warn("User with id={} did not like", userId);
+            throw new NotFoundException("Пользователь id=" + id + " не ставил лайк");
+        }
+        log.info("like id={} remove", id);
+        likes.remove(userId);
+    }
+
+    public List<Film> showPopularFilms() {
+        Map<Long, Film> films = filmStorage.getFilms();
+        log.info("Method started (showPopularFilms)");
+        return films.values().stream()
+                .sorted((x, y) -> y.getLikes().size() - x.getLikes().size())
+                .limit(10)
+                .toList();
+    }
+
+    public List<Film> showPopularFilms(Long count) {
+        Map<Long, Film> films = filmStorage.getFilms();
+        log.info("Method started (showPopularFilms)");
+        return films.values().stream()
+                .sorted((x, y) -> y.getLikes().size() - x.getLikes().size())
+                .limit(count)
+                .toList();
+    }
+}
